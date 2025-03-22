51f34e84b0253607a8a42a61ec75ae3ee00b0bcc
AKP <abi@tdpain.net>
2025-03-22 17:10:08 +0000

    Integrate menu and game

--- game/__main__.py
+++ game/__main__.py
@@ -5,42 +5,34 @@ import gameplay
 
     
 def main():
-    # pygame setup
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
-    
-    #surface1 = pygame.set_mode((1280,720))
-    clock = pygame.time.Clock()
     running = True
     pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-    colour = "lightblue"
     screen.fill("lightblue")
-    g = gameplay.GamePlay(screen)
-    m = menu.Menu(screen)
+
+    active_scene = menu.Menu(screen)
+
     while running:
-        g.do()
-        #Image = Buttonify('resources\sprites\start.png',(100,100),screen)
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         for _ in pygame.event.get(eventtype=pygame.QUIT):
             running = False
 
         pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
         for event in pygame.event.get(eventtype=util.TRANSITION_EVENT_TYPE):
-            raise NotImplementedError("switch to scene " + event.dict["to"])
+            next_scene = event.dict["to"]
+            match next_scene:
+                case "menu":
+                    active_scene = menu.Menu(screen)
+                case "gameplay":
+                    active_scene = gameplay.GamePlay(screen)
+                case _:
+                    raise NotImplementedError("switch to scene " + event.dict["to"])
+            screen.fill(0x000000)
 
-        m.do()
-
-        # flip() the display to put your work on screen
+        active_scene.do()
         pygame.display.flip()
-
-        # limits FPS to 60
-        # dt is delta time in seconds since last frame, used for framerate-
-        # independent physics.
-        dt = clock.tick(60) / 1000
 
     pygame.quit()
 
